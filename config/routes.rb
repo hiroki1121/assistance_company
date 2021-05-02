dc97b20b11522f71244b67c3cf399f6bf1efa15f
hiroki1121 <81612317+hiroki1121@users.noreply.github.com>
2021-05-02 15:57:43 +0900

    devise導入〜バリデーション作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :contracted_side_users
+  devise_for :consignment_side_users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/models/contracted_side_user_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ContractedSideUserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
