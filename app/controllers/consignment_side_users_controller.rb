6f27ca5ab7329567163c9ad3b170a65fab9b02c0
hiroki1121 <81612317+hiroki1121@users.noreply.github.com>
2021-05-10 11:07:49 +0900

    マイページバリデーション

--- app/controllers/consignment_side_users_controller.rb
+++ app/controllers/consignment_side_users_controller.rb
@@ -1,6 +1,14 @@
 class ConsignmentSideUsersController < ApplicationController
+  before_action authenticate_any!
+
   def show
     @favorite = Favorite.find_by(consignment_side_user_id: current_consignment_side_user.id)
     @posts = Post.where(params[id: @favorite.post_id]) if @favorite.present?
+  end
+
+  private
+
+  def authenticate_any!
+    redirect_to consignment_side_user_session_path unless consignment_side_user_signed_in?
   end
 end
--- app/controllers/contracted_side_users_controller.rb
+++ app/controllers/contracted_side_users_controller.rb
@@ -1,5 +1,12 @@
 class ContractedSideUsersController < ApplicationController
+  before_action :authenticate_any!
   def show
     @post = Post.where(params[contracted_side_user_id: current_contracted_side_user.id])
+  end
+
+  private
+
+  def authenticate_any!
+    redirect_to contracted_side_user_session_path unless contracted_side_user_signed_in?
   end
 end
