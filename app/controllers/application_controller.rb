4d6d3a6a012044894f7daf7f91964ab138c2bc71
hiroki1121 <81612317+hiroki1121@users.noreply.github.com>
2021-05-10 20:05:07 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
@@ -10,5 +11,13 @@ class ApplicationController < ActionController::Base
                                              :first_name_ruby,
                                              :company_name,
                                              :phone_number])
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
