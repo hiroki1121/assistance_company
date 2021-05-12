4d35a1fef11213a71d9c7ce3893cbddd3fab56b8
hiroki1121 <81612317+hiroki1121@users.noreply.github.com>
2021-05-12 12:04:23 +0900

    reviewルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :consignment_side_users, only: [:show]
   resources :contracted_side_users, only: [:show]
   resources :posts, only: [:index, :new, :create, :edit, :update, :destroy], shallow: true do
+    resource :reviews, only: [:new, :create, :show, :edit, :update, :destroy]
     resource :favorites, only: [:create, :destroy, :show]
     collection do
       get 'search'
