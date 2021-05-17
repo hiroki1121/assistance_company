07fc3466726d44dab50683d98ddbd2a444973315
hiroki1121 <81612317+hiroki1121@users.noreply.github.com>
2021-05-17 14:54:26 +0900

    インスタンスへ代入

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -14,7 +14,7 @@ class FavoritesController < ApplicationController
   end
 
   def show
-    Favorite.find_by(post_id: @post.id)
+    @favorite = Favorite.find_by(post_id: @post.id)
     @review = Review.where(post_id: @post.id)
   end
 
