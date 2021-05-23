ece84be11ed7bd63558d365c8eec1d245f88510f
hiroki1121 <81612317+hiroki1121@users.noreply.github.com>
2021-05-23 20:41:50 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,3 +60,6 @@
 #     # password: "please use keys"
 #   }
 server '54.178.64.215', user: 'ec2-user', roles: %w{app db web}
+if ($http_x_forwarded_proto = 'http'){
+  return 301 https://www.assistance-company.com;
+}
